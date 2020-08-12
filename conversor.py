984ddb8ff901f0c859a2f91e9632f151eefcc562
Andrea Jurado <andre.jurado.n@gmail.com>
2020-08-12 08:04:55 -0500

    Se han agregado un camibo de dolares a pesos

--- conversor.py
+++ conversor.py
@@ -1,6 +1,18 @@
-pesos = input("¿Cuantos pesos colombianos tienes?")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = str(dolares)
-print("Tienes $" + dolares + "dólares")
+opcion = input("Conversor de moneda\nEscoja: \n1.Cambiar de pesos colombianos a dólares \n2.Pasar de dólares a pesos colombianos")
+
+if opcion == '1':
+  pesos = input("¿Cuantos pesos colombianos tienes?")
+  pesos = float(pesos)
+  valor_dolar = 3875
+  dolares = pesos / valor_dolar
+  dolares = round(dolares, 2)
+  dolares = str(dolares)
+  print("Tienes $" + dolares + "dólares")
+elif opcion == '2' :
+  dolares = input("¿Cuantos dólares  tienes?")
+  dolares = float(dolares)
+  valor_pesos = 1/3875
+  pesos = dolares / valor_pesos
+  pesos = round(pesos, 2)
+  pesos = str(pesos)
+  print("Tienes $" + pesos + "pesos")
