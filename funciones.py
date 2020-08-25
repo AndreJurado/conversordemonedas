a02847816bec468e22dc7fd058765a0b4bbbeed1
Andrea Jurado <andre.jurado.n@gmail.com>
2020-08-25 10:12:26 -0500

    añadido de archivo de funciones

---
+++ funciones.py
@@ -0,0 +1,25 @@
+# def imprimir_mensaje():
+#   print("Mensaje especial")
+#   print("Estoy aprendiendo a usar funciones")
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+def conversacion(mensaje):
+  print("hola")
+  print("como estas")
+  print(mensaje)
+  print ("adios")
+
+
+
+
+opcion = int(input("Elige una opcion (1,2,3)"))
+
+if opcion == 1:
+  conversacion("elegiste la 1")
+elif opcion == 2:
+  conversacion("elegiste la 2")
+elif opcion == 3:
+  conversacion("elegiste la 3")
